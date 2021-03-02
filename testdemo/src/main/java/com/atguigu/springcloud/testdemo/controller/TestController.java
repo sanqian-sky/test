db2ce85894ee6bf6850b9ccc31b820a7b1a80954
wangfuming <1780979562@qq.com>
2021-03-02 22:40:35 +0800

    创建了testcontroller

--- testdemo/src/main/java/com/atguigu/springcloud/testdemo/controller/TestController.java
+++ testdemo/src/main/java/com/atguigu/springcloud/testdemo/controller/TestController.java
@@ -1,4 +1,11 @@
 package com.atguigu.springcloud.testdemo.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+
+    public String test(){
+        return "test";
+    }
 }
